a6d1e5324b2b6ad94a005bc6db3fce6dabe5d8f4
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-06-26 01:31:10 +0200

    completed info site project

--- src/Info.jsx
+++ src/Info.jsx
@@ -6,8 +6,26 @@ export default function Info() {
       <h3 className="name">Kgotso Makhalimele</h3>
       <p className="description">Software Developer</p>
       <div className="contact-btn">
-        <sl-button size="small">Email</sl-button>
-        <sl-button size="small">LinkedIn</sl-button>
+        <sl-button size="small">
+          <sl-icon name="envelope-at"></sl-icon> Email{" "}
+        </sl-button>
+        <sl-button size="small">
+          <sl-icon name="github"></sl-icon> GitHub
+        </sl-button>
+        <sl-button size="small">
+          <sl-icon name="linkedin"></sl-icon> LinkedIn
+        </sl-button>
+      </div>
+      <div>
+        <h3 className="sum-head">About</h3>
+        <p className="text">
+          I am an aspiring full-stack developer with a passion for learning and
+          growing my skills. My aim is to contribute to projects that have an
+          impact.
+        </p>
+        <h3 className="sum-head">Interests</h3>
+        <p className="text">My interests outside tech include keeping up to date with the latest sports and listening to DarkNet Diaries or 
+        the Joe Rogan podcast. A fun fact about me is that my favourite animal is a honey badger.</p>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Info from './Info.jsx'
 import Picture from './Picture.jsx'
-
+import Footer from './Footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Picture />
     <Info />
+    <Footer />
   </React.StrictMode>,
 )
